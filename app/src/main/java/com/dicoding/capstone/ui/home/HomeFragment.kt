6d86224865ddc33714db3e65739ccb017fb12a3b
ahmad <ahmad.mukhlas61@gmail.com>
2023-06-02 17:21:56 +0700

    "slicing ui for profile, login, register"

--- app/src/main/java/com/dicoding/capstone/ui/home/HomeFragment.kt
+++ app/src/main/java/com/dicoding/capstone/ui/home/HomeFragment.kt
@@ -22,15 +22,15 @@ private var _binding: FragmentHomeBinding? = null
     savedInstanceState: Bundle?
   ): View {
     val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        ViewModelProvider(this)[HomeViewModel::class.java]
 
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textHome
-    homeViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
-    }
+//    val textView: TextView = binding.textHome
+//    homeViewModel.text.observe(viewLifecycleOwner) {
+//      textView.text = it
+//    }
     return root
   }
 
---
+++ app/src/main/java/com/dicoding/capstone/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.capstone.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.capstone.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
